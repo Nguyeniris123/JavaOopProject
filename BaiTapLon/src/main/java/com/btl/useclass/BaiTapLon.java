9d389576ca249f3fd5b6fd6c111656ef024bea66
Hồ Chí Nguyên <136091628+Nguyeniris123@users.noreply.github.com>
2023-12-15 21:51:01 +0700

    Tạo

---
+++ BaiTapLon/src/main/java/com/btl/useclass/BaiTapLon.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.btl.useclass;
+
+/**
+ *
+ * @author NGUYEN
+ */
+public class BaiTapLon {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
