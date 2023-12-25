e9267b5e4e725d45fe24cabef10743d795e9499f
TinyLove123 <142028792+TinyLove123@users.noreply.github.com>
2023-12-26 00:30:14 +0700

    them 1234

--- BaiTapLon/src/main/java/com/btl/dh22it01/Btl.java
+++ BaiTapLon/src/main/java/com/btl/dh22it01/Btl.java
@@ -11,5 +11,7 @@ package com.btl.dh22it01;
 public class Btl {
     public static void main(String[] args) {
         Menu mn = new Menu();
+        System.out.print("123 ");
+                
     }
 }
